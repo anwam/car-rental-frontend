4de53873bf737d3ab799bb94f4365e415ba96a9e
anwam <dev.anuwong@gmail.com>
2024-02-11 10:05:35 +0700

    feat: integrate add car and invalidate cache

--- src/components/FormAddCar.jsx
+++ src/components/FormAddCar.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 function FormAddCar({ isOpen, setIsOpen }) {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [discount, setDiscount] = useState("");
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (newCar) =>
+      fetch("http://localhost:1323/cars", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newCar),
+      }).then((res) => res.json()),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["carList"] });
+    },
+  });
+
+  const handleAddClick = (e) => {
+    e.preventDefault();
+    const newCar = {
+      name: name,
+      price: Number(price),
+      discount: Number(discount),
+    };
+    mutation.mutate(newCar);
+    setIsOpen(false);
+  };
+
   return (
     <dialog
       open={isOpen}
@@ -17,20 +50,32 @@ function FormAddCar({ isOpen, setIsOpen }) {
           className="p-3 rounded-md"
           type="text"
           placeholder="Name"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
         />
         <label htmlFor="price">Price</label>
         <input
           id="price"
           className="p-3 rounded-md"
-          type="text"
+          type="number"
           placeholder="Price"
+          value={Number(price).toString()}
+          onChange={(e) => {
+            setPrice(Number(e.target.value).toString());
+          }}
         />
         <label htmlFor="discount">Discount</label>
         <input
           id="discount"
           className="p-3 rounded-md"
-          type="text"
-          placeholder="discount"
+          type="number"
+          placeholder="Discount"
+          value={Number(discount).toString()}
+          onChange={(e) => {
+            setDiscount(Number(e.target.value).toString());
+          }}
         />
         <div className="flex flex-row">
           <button
@@ -39,7 +84,10 @@ function FormAddCar({ isOpen, setIsOpen }) {
           >
             Cancel
           </button>
-          <button className="bg-primary rounded-md px-3 py-2 text-sm text-white">
+          <button
+            className="bg-primary rounded-md px-3 py-2 text-sm text-white"
+            onClick={handleAddClick}
+          >
             Add
           </button>
         </div>
